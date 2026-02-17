f90109bd63bce684a2691caf556a5706568d317b
Mika Oja <enk@kapsi.fi>
2026-02-17 12:57:08 +0200

    Added entrypoint

--- sensorhub/api.py
+++ sensorhub/api.py
@@ -3,10 +3,12 @@ from flask_restful import Api
 
 from .resources.sensor import SensorCollection, SensorItem
 from .resources.measurement import MeasurementCollection
+from . import views
 
 api_bp = Blueprint("api", __name__, url_prefix="/api")
 api = Api(api_bp)
 
+api_bp.add_url_rule("/", "entry", views.entry)
 api.add_resource(SensorCollection, "/sensors/")
 api.add_resource(SensorItem, "/sensors/<sensor:sensor>/")
 api.add_resource(MeasurementCollection, "/sensors/<sensor:sensor>/measurements/")
---
+++ sensorhub/views.py
@@ -0,0 +1,2 @@
+def entry():
+    return {"api_version": "1.0", "api_name": "sensorhub"}
