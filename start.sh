e979ed9a41acba74192842b348afd3e29363f5ea
Safi <safi50@Safis-MacBook-Pro.local>
2026-03-26 23:42:19 +0200

    fixed error

--- start.sh
+++ start.sh
@@ -4,4 +4,4 @@ flask --app=sensorhub init-db
 flask --app=sensorhub testgen
 flask --app=sensorhub masterkey > /tmp/masterkey.txt
 
-gunicorn -w 3 -b 0.0.0.0 sensorhub:create_app()
+exec gunicorn -w 3 -b 0.0.0.0:8000 'sensorhub:create_app()'
